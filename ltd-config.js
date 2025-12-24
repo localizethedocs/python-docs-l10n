2f818716e84d46e9bc6d616e232eae19d7a469ca
ltdrobot <ltdrobot@localizethedocs.org>
2025-12-24 08:51:47 +0000

    Deploying to pages from @ localizethedocs/python-docs-l10n@45a59214b22e0c9e9c33f0f40edd10a970744652 🚀

---
+++ ltd-config.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+    ["3.14",    "Release 3.14"],
+    ["3.13",    "Release 3.13"],
+    ["3.12",    "Release 3.12"],
+    ["3.11",    "Release 3.11"],
+    ["3.10",    "Release 3.10"],
+    ["3.9",     "Release 3.9"],
+    ["3.8",     "Release 3.8"],
+    ["3.7",     "Release 3.7"],
+    ["3.6",     "Release 3.6"],
+    ["3.5",     "Release 3.5"],
+    ["3.4",     "Release 3.4"],
+    ["3.3",     "Release 3.3"],
+    ["3.2",     "Release 3.2"],
+    ["3.1",     "Release 3.1"],
+    ["3.0",     "Release 3.0"],
+    ["2.7",     "Release 2.7"],
+    ["2.6",     "Release 2.6"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/python-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/python-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/python-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/python-docs-l10n"],
+  ]
+};
